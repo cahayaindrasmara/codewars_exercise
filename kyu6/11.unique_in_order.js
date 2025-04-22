504e55cd31052f6e66cff64e3740fe41f542f26e
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-04-22 07:48:01 +0700

    add kyu6 dan kyu8

---
+++ kyu6/11.unique_in_order.js
@@ -0,0 +1,19 @@
+function uniqueInOrder(iterable) {
+    let listUnique = [];
+
+    for (let i = 0; i < iterable.length; i++) {
+        let found = false;
+
+        if (iterable[i] === iterable[i + 1]) {
+            found = true;
+        }
+
+        if (!found) {
+            listUnique.push(iterable[i])
+        }
+    }
+
+    return listUnique;
+}
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'));
---
+++ kyu6/12.dna_strand.js
@@ -0,0 +1,21 @@
+function dnaStrand(dna) {
+    let result = "";
+
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] === "A") {
+            result += "T";
+        } else if (dna[i] === "T") {
+            result += "A";
+        } else if (dna[i] === "G") {
+            result += "C";
+        } else if (dna[i] === "C") {
+            result += "G";
+        }
+    }
+
+    return result;
+}
+
+console.log(dnaStrand("AAAA")); //TTTT
+console.log(dnaStrand("ATTGC")); //TAACG
+console.log(dnaStrand("GTAT")); //CATA
