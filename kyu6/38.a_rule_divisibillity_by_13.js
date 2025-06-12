837b5312c2d6da78187ca48d0798f09cc0b4bf2e
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-06-12 10:12:33 +0700

    add doc file 12-06-25

--- kyu6/38.a_rule_divisibillity_by_13.js
+++ kyu6/38.a_rule_divisibillity_by_13.js
@@ -19,6 +19,12 @@ function thirt(n) {
     }
 
     return n;
+
+    /*
+     const nums = [1,10,9,12,3,4]
+     var sum = (''+n).split('').reverse().reduce((sum,v,i) => sum + v * nums[i%nums.length], 0)
+     return sum === n ? n : thirt(sum)
+    */
 }
 
 console.log(thirt(8529)); //79
