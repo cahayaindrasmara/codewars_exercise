5b3cf8e9c041330a82bb348f53d909b6af59f430
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-06-25 07:56:05 +0700

    update kyu6

--- kyu6/07.detect_pangram.js
+++ kyu6/07.detect_pangram.js
@@ -1,14 +1,17 @@
 function isPangram(string) {
-    let lowercase = string.toLowerCase();
+    // let lowercase = string.toLowerCase();
 
-    for (let i = 97; i <= 122; i++) {
-        let char = String.fromCharCode(i);
-        if (!lowercase.includes(char)) {
-            return false;
-        }
-    }
+    // for (let i = 97; i <= 122; i++) {
+    //     let char = String.fromCharCode(i);
+    //     if (!lowercase.includes(char)) {
+    //         return false;
+    //     }
+    // }
 
-    return true;
+    // return true;
+
+    //kiss
+    return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
 }
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog.")); //true
