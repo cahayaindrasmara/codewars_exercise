7f1ac74ecbe96185b0d1be32be5e6b3342b63f63
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-06-27 08:16:08 +0700

    update kyu5  07. regex password validation

--- kyu5/07.regex_password_validation.js
+++ kyu5/07.regex_password_validation.js
@@ -1,5 +1,16 @@
 function validPass(string) {
     return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(string);
+
+    /*
+    (?=...) adalah positive lookahead
+    Di posisi saat ini, kita mengecek apakah karakter setelahnya sesuai dengan syarat tertentu.
+
+    ^.{6,}$
+    string yang memiliki minimal 6 karakter apa pun dari awal sampai akhir.
+
+    (?=.*[a-z])
+    digunakan untuk mengecek apakah di dalam string terdapat minimal satu huruf kecil (a sampai z), di posisi mana pun dalam string.
+    */
 }
 
 console.log(validPass("fjd3IR9"))
