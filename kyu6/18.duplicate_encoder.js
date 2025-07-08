581f7df7b9f1accbb95df0d075c7d8aeb45e92ad
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-07-08 07:27:50 +0700

    update 1. duplicate encoder

--- kyu6/18.duplicate_encoder.js
+++ kyu6/18.duplicate_encoder.js
@@ -1,26 +1,25 @@
 function duplicateEncode(word) {
-    let result = "";
     let lowerCase = word.toLowerCase();
+    let chars = {};
+    let result = "";
 
-    for (let i = 0; i < word.length; i++) {
-        let count = 0;
-        console.log("#1:", word[i]);
-        for (let j = 0; j < word.length; j++) {
-            console.log("#2:", word[j])
-            if (lowerCase[i] === lowerCase[j]) {
-                count++
-            }
-        }
-        if (count === 1) {
-            result += "(";
-        } else {
+    for (let i = 0; i < lowerCase.length; i++) {
+        chars[lowerCase[i]] = (chars[lowerCase[i]] || 0) + 1;
+    };
+
+    for (let i = 0; i < lowerCase.length; i++) {
+        if (chars[lowerCase[i]] > 1) {
             result += ")"
+        } else {
+            result += "("
         }
     }
+
+    console.log(chars)
     return result;
 }
 
-console.log(duplicateEncode("din"));
-console.log(duplicateEncode("recede"));
-console.log(duplicateEncode("Success")); //)())())
-console.log(duplicateEncode("(( @")); //))((
+console.log(duplicateEncode("din")); // (((
+console.log(duplicateEncode("recede")); // ()()()
+console.log(duplicateEncode("Success")); // )())())
+console.log(duplicateEncode("(( @")); // ))((
