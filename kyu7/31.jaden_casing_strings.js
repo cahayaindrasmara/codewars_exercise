55a4352df4b397b44da23bf12753964cf5de9ee1
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-07-14 06:40:27 +0700

    update kyu7: 31.Jaden Casing String

--- kyu7/31.jaden_casing_strings.js
+++ kyu7/31.jaden_casing_strings.js
@@ -4,7 +4,19 @@ Object.defineProperty(
     {
         value:
             function toJadenCase() {
-                return this.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+                // return this.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
+
+                let result = "";
+
+                for (let i = 0; i < this.length; i++) {
+                    if (i === 0 || this[i - 1] === " ") {
+                        result += this[i].toUpperCase()
+                    } else {
+                        result += this[i]
+                    }
+                }
+
+                return result;
             }
     }
 )
