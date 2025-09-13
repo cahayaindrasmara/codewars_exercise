3e56321fc5508605f34f7b627182ed76c81c9602
Cahaya Indrasmara <indrasmara.cahaya27@gmail.com>
2025-09-13 18:08:24 +0700

    update file week 2 sep

--- kyu8/226.geometry_basics_distance_between_points_in_2D.js
+++ kyu8/226.geometry_basics_distance_between_points_in_2D.js
@@ -8,6 +8,9 @@ function distanceBetweenPoints(a, b) {
     console.log("x1:", x1, "y1:", y1, "x2:", x2, "y2:", y2);
 
     return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+    //kiss
+    //return Math.hypot(a.x - b.x, a.y - b.y);
 }
 
 console.log(distanceBetweenPoints(new Point(1, 6), new Point(4, 2)))
